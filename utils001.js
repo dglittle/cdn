449a1baaf095dc809606993fe9c52f03e8a60af0
dglittle <glittle@gmail.com>
2018-01-06 17:59:13 -0800

    my commit message

---
+++ utils001.js
@@ -0,0 +1,17 @@
+
+each = function (o, cb) {
+    if (o instanceof Array) {
+        for (var i = 0; i < o.length; i++) {
+            if (cb(o[i], i, o) == false)
+                return false
+        }
+    } else {
+        for (var k in o) {
+            if (o.hasOwnProperty(k)) {
+                if (cb(o[k], k, o) == false)
+                    return false
+            }
+        }
+    }
+    return true
+}
