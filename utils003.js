530759fd98c938aa6a07beefb00c6dde45a14a3c
dglittle <glittle@gmail.com>
2018-02-01 17:17:31 -1000

    my commit message

---
+++ utils003.js
@@ -0,0 +1,45 @@
+
+each = function (o, cb) {
+    if (o instanceof Array) {
+        for (var i = 0; i < o.length; i++) {
+            if (cb(o[i], i, o) == false)
+                return false
+        }
+    } else {
+        for (var k in o) {
+            if (o.hasOwnProperty(k)) {
+                if (cb(o[k], k, o) == false)
+                    return false
+            }
+        }
+    }
+    return true
+}
+
+lerp = function (t0, v0, t1, v1, t) {
+    return (t - t0) * (v1 - v0) / (t1 - t0) + v0
+}
+
+map = function (o, func) {
+    if (o instanceof Array) {
+        var accum = []
+        for (var i = 0; i < o.length; i++)
+            accum[i] = func(o[i], i, o)
+        return accum
+    } else {
+        var accum = {}
+        for (var k in o)
+            if (o.hasOwnProperty(k))
+                accum[k] = func(o[k], k, o)
+        return accum
+    }
+}
+
+wget = function (url, cb) {
+    var r = new XMLHttpRequest()
+    r.addEventListener("load", function () {
+        cb(this.responseText)
+    })
+    r.open("GET", url)
+    r.send()
+}
